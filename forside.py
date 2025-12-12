6120e1f8bd8351a11aef0d271d5e3d67797fdd3e
FayssalRF <frf@mover.dk>
2025-12-12 11:20:00 +0100

    Simplify instruction text in forside.py

--- forside.py
+++ forside.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 def forside_tab():
     st.markdown("<div style='text-align: center;'><h1>Customer Success Automation site</h1></div>", unsafe_allow_html=True)
-    st.markdown("<div style='text-align: center;'><p>Vælg et menupunkt i fanebjælken ovenfor for at komme videre.</p></div>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: center;'><p>Vælg et menupunkt for at komme videre.</p></div>", unsafe_allow_html=True)
